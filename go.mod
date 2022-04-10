40e1ff0712957f88e48a0ffe25c8654a7ce6ab3d
jiurongzhao <1053668864@qq.com>
2022-04-10 20:17:59 +0800

    feat: redis plugin demo

--- go.mod
+++ go.mod
@@ -2,21 +2,25 @@ module github.com/jsmzr/bootstrap-plugin-example
 
 go 1.17
 
-require github.com/jsmzr/bootstrap-plugin-logrus v0.0.1
+require github.com/jsmzr/bootstrap-plugin-logrus v0.0.2
 
-require github.com/jsmzr/bootstrap-plugin-config-yaml v0.0.1
+require github.com/jsmzr/bootstrap-plugin-config-yaml v0.0.2
 
-require github.com/jsmzr/bootstrap-plugin v0.0.1
+require github.com/jsmzr/bootstrap-plugin v0.0.2
 
 require (
-	github.com/jsmzr/bootstrap-config v0.0.1
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/jsmzr/bootstrap-config v0.0.2
 	github.com/jsmzr/bootstrap-log v0.0.2
+	github.com/jsmzr/bootstrap-plugin-redis v0.0.1
 )
 
 require (
-	github.com/jsmzr/bootstrap-config-yaml v0.0.2 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/jsmzr/bootstrap-config-yaml v0.0.3 // indirect
 	github.com/jsmzr/bootstrap-log-logrus v0.0.3 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/jsmzr/bootstrap-config/config"
 	"github.com/jsmzr/bootstrap-log/log"
 	_ "github.com/jsmzr/bootstrap-plugin-config-yaml/yaml"
 	_ "github.com/jsmzr/bootstrap-plugin-logrus/logrus"
+
+	_ "github.com/jsmzr/bootstrap-plugin-redis/redis"
+	// _ "github.com/jsmzr/bootstrap-plugin-redis/cluster"
+	"github.com/jsmzr/bootstrap-plugin-redis/connection"
+
 	"github.com/jsmzr/bootstrap-plugin/plugin"
 )
 
@@ -26,5 +34,30 @@ func main() {
 	var configResource ConfigResource
 	config.Resolve("bootstrap.example", &configResource)
 	log.Info("get config resource %v", configResource)
+	clusterConfig, ok := config.Get("bootstrap.redis.cluster.addrs")
+	if !ok {
+		return
+	}
+	log.Info("get bootstrap.redis.cluster:%v", clusterConfig)
+	var clusterOptions redis.ClusterOptions
+	if err := config.Resolve("bootstrap.redis.cluster", &clusterOptions); err != nil {
+		fmt.Println(err)
+		return
+	}
+	log.Info("resolve: %v", clusterOptions)
+
+	// conn := connection.GetClutserClient()
+	conn := connection.GetClient()
+	key := "test-1"
+	value := "v1"
+	if err := conn.Set(context.Background(), key, value, time.Hour).Err(); err != nil {
+		log.Error("redis set faied", err)
+		return
+	}
+	if result, err := conn.Get(context.Background(), key).Result(); err != nil {
+		log.Error("redis get failed", err)
+	} else {
+		log.Info("redis get value: %v", result)
+	}
 
 }
