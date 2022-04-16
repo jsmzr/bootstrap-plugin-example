90a315e0da2143324348f94ec417ff3a3525e424
jiurongzhao <1053668864@qq.com>
2022-04-16 20:17:40 +0800

    feat: 新的配置实现

--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.17
 
 require github.com/jsmzr/bootstrap-plugin-logrus v0.0.2
 
-require github.com/jsmzr/bootstrap-plugin-config-yaml v0.0.2
+require github.com/jsmzr/bootstrap-plugin-config-yaml v0.0.3
 
 require github.com/jsmzr/bootstrap-plugin v0.0.2
 
 require (
 	github.com/go-redis/redis/v8 v8.11.5
-	github.com/jsmzr/bootstrap-config v0.0.2
+	github.com/jsmzr/bootstrap-config v0.0.3
 	github.com/jsmzr/bootstrap-log v0.0.2
 	github.com/jsmzr/bootstrap-plugin-redis v0.0.1
 )
@@ -18,9 +18,12 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/jsmzr/bootstrap-config-yaml v0.0.3 // indirect
+	github.com/jsmzr/bootstrap-config-yaml v0.0.4 // indirect
 	github.com/jsmzr/bootstrap-log-logrus v0.0.3 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/tidwall/gjson v1.14.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,10 +10,11 @@ import (
 	"github.com/jsmzr/bootstrap-log/log"
 	_ "github.com/jsmzr/bootstrap-plugin-config-yaml/yaml"
 	_ "github.com/jsmzr/bootstrap-plugin-logrus/logrus"
-
-	_ "github.com/jsmzr/bootstrap-plugin-redis/redis"
-	// _ "github.com/jsmzr/bootstrap-plugin-redis/cluster"
 	"github.com/jsmzr/bootstrap-plugin-redis/connection"
+
+	// _ "github.com/jsmzr/bootstrap-plugin-redis/redis"
+	//_ "github.com/jsmzr/bootstrap-plugin-redis/cluster"
+	// _ "github.com/jsmzr/bootstrap-plugin-redis/sentinel"
 
 	"github.com/jsmzr/bootstrap-plugin/plugin"
 )
@@ -29,24 +30,61 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	log.Info("pulgin init success %s", "123321")
-	log.Info("pulgin init success %d", 123456)
-	var configResource ConfigResource
-	config.Resolve("bootstrap.example", &configResource)
-	log.Info("get config resource %v", configResource)
-	clusterConfig, ok := config.Get("bootstrap.redis.cluster.addrs")
-	if !ok {
-		return
+	logDemo()
+	configDemo()
+	// 需要配置本地的 redis 信息
+	// redisDemo()
+}
+
+func logDemo() {
+	log.Info("string value: %s", "123321")
+	log.Info("int value: %d", 123456)
+	log.Info("bool value: %v", true)
+	log.Info("float value: %f", 3.14)
+	log.Info("struct value: %v", ConfigResource{
+		Config: "foo",
+		Log:    "boo",
+	})
+
+	log.Debug("debug log")
+	log.Info("info log")
+	log.Warn("warn log")
+	log.Error("error log")
+}
+
+func configDemo() {
+	if v, ok := config.Get("bootstrap.example.int"); ok {
+		log.Info("config int value: %d", v.Int())
 	}
-	log.Info("get bootstrap.redis.cluster:%v", clusterConfig)
+	if v, ok := config.Get("bootstrap.example.float"); ok {
+		log.Info("config float value: %f", v.Float())
+	}
+	if v, ok := config.Get("bootstrap.example.string"); ok {
+		log.Info("config float value: %s", v.String())
+	}
+	if v, ok := config.Get("bootstrap.example.bool"); ok {
+		log.Info("config float value: %v", v.Bool())
+	}
+
+	var exampleConfig ConfigResource
+	if err := config.Resolve("bootstrap.example", &exampleConfig); err != nil {
+		log.Warn("can't resolve bootstrap.example")
+	} else {
+		log.Info("config struct value: %v", exampleConfig)
+	}
+
 	var clusterOptions redis.ClusterOptions
 	if err := config.Resolve("bootstrap.redis.cluster", &clusterOptions); err != nil {
-		fmt.Println(err)
-		return
+		log.Warn("can't resolve bootstrap.redis.cluster")
+	} else {
+		log.Info("resolve: %v", clusterOptions)
 	}
-	log.Info("resolve: %v", clusterOptions)
+}
 
+func redisDemo() {
+	// 需要获取集群连接则在 import 选择导入 github.com/jsmzr/bootstrap-plugin-redis/cluster
 	// conn := connection.GetClutserClient()
+	// 需要获取单机或哨兵下的连接则在 import 选择导入 github.com/jsmzr/bootstrap-plugin-redis/redis 或 sentinel
 	conn := connection.GetClient()
 	key := "test-1"
 	value := "v1"
@@ -54,10 +92,10 @@ func main() {
 		log.Error("redis set faied", err)
 		return
 	}
+	log.Info("redis set %s=%s", key, value)
 	if result, err := conn.Get(context.Background(), key).Result(); err != nil {
 		log.Error("redis get failed", err)
 	} else {
-		log.Info("redis get value: %v", result)
+		log.Info("redis get %s value: %s", key, result)
 	}
-
 }
